cb46f359fa2c856fbe0455482b123092525de9cb
Olivier Barbier <obarbier@cisco.com>
2019-02-03 12:50:51 -0500

    this is it

--- day1.py
+++ day1.py
@@ -16,8 +16,21 @@ X[ : , 1:3] = imputer.transform(X[ : , 1:3])
 print(X)
 print("Encoding Categorical Data")
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer, make_column_transformer
 labelencoder_X = LabelEncoder()
 X[ : , 0] = labelencoder_X.fit_transform(X[ : , 0])
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
-print(X)
+preprocess = make_column_transformer(
+    ( OneHotEncoder(categories='auto'),[0])
+)
+X = preprocess.fit_transform(X)
+labelencoder_Y = LabelEncoder()
+Y =  labelencoder_Y.fit_transform(Y)
+print("Splitting the datasets into training sets and Test sets")
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
+print("Feature Scaling")
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
+print(X_train)
